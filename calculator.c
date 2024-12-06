8044a41d6649f8e92875ced3989c291a04f1ac6f
Vatsal1204 <pitrodavatsal1242005@gmail.com>
2024-12-06 11:27:17 +0530

    calculator

--- calculator.c
+++ calculator.c
@@ -3,22 +3,23 @@ void main(){
     int a;
     printf("enter a number less than or equal to 100:");
     scanf("%d",&a);
-    if(90<a<=100){
-        printf("excellent work:A");
+    if(a>90 && a<=100){
+        printf("excellent work:grade A");
     }
-        else if(80<a<90){
-        printf("good work:B");
+        else if(a>80 && a<=90){
+        printf("good work:grade B");
         }
-        else if(70<a<80){
-            printf("good but try hard:C");
+        else if(a>70 && a<=80){
+            printf("good but try hard:grade C");
         }
-        else if (60<a<70){
-            printf("average work:D");
+        else if (a>60 && a<=70){
+            printf("average work:grade D");
         }
-        else if(40<a<60){
-            printf("bad work:E");
+        else if(a>=40 && a<=60){
+            printf("bad work:grade E");
         }
         else{
             printf("fail:F");
         }
     }
+
